aee45e58340da8e510e0b51aeef9d9418712eebe
lux <kanevegor@icloud.com>
2023-08-17 02:11:45 +0300

    lil fix

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from telebot import TeleBot
 from env import env
 
-bot_token = '6652191564:AAFYNcvkenATwD7KAwZLOnpEJ-yflcuHq5o'
+bot_token = env.telegram_token
 bot = TeleBot(bot_token, parse_mode="html")
